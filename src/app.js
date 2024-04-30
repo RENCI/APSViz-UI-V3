241bb8b6c0b0260e2d01ad510c0697bdb1d9f958
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-04-30 14:10:05 -0400

    adding a floating dialog and using the App component to test it.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import React, { Fragment } from 'react';
 import { Map } from '@components/map';
+import FloatingDialog from '@utils/dialog-utils';
+// import SubObj from "@utils/subObj";
 
 export const App = () => {
   return (
     <Fragment>
-      <Map />
-    </Fragment>
+        <FloatingDialog title={"A title"} dialogText={"the dialog text"} openDialog={true}/>
+        <Map />
+     </Fragment>
   );
 };
---
+++ src/utils/dialog-utils.js
@@ -0,0 +1,87 @@
+import React, { Fragment } from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+import Paper from '@mui/material/Paper';
+import Draggable from 'react-draggable';
+import PropTypes from 'prop-types';
+import Slide from '@mui/material/Slide';
+
+/**
+ * This creates a 3D dialog.
+ *
+ * @param props
+ * @returns {JSX.Element}
+ * @constructor
+ */
+function PaperComponent(props) {
+  return (
+    <Draggable handle="#draggable-dialog-title" cancel={'[class*="MuiDialogContent-root"]'}>
+      <Paper {...props} />
+    </Draggable>
+  );
+}
+
+/**
+ * This creates an animated transition for the dialog that pops up
+ * @type {React.ForwardRefExoticComponent<React.PropsWithoutRef<{}> & React.RefAttributes<any>>}
+ */
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+/**
+ * This is the floating component that displays a floating dialog
+ *
+ * @param title
+ * @param dialogText
+ * @param openDialog
+ * @returns {JSX.Element}
+ * @constructor
+ */
+export default function FloatingDialog({ title, dialogText, openDialog}) { //, dialogObject
+  const [open, setOpen] = React.useState(openDialog);
+
+  // closes the dialog
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  // return the component that is rendered
+  return (
+    <Fragment>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        PaperComponent={PaperComponent}
+        TransitionComponent={Transition}
+        aria-labelledby="draggable-dialog-title"
+      >
+        <DialogTitle style={{ cursor: 'move' }} id="draggable-dialog-title">
+          { title }
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            { dialogText }
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button autoFocus onClick={handleClose}>
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </Fragment>
+  );
+};
+
+// define the properties of this component
+FloatingDialog.propTypes = {
+  title: PropTypes.string,
+  dialogText: PropTypes.string,
+  openDialog: PropTypes.bool
+  // dialogObject: PropTypes.any
+};
