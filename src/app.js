370098609840a1cc1f00106bc7e8aa26adaa127b
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-05-01 10:48:16 -0400

    reverting back to the original

--- src/app.js
+++ src/app.js
@@ -1,26 +1,10 @@
 import React, { Fragment } from 'react';
 import { Map } from '@components/map';
-import BaseFloatingDialog from "@utils/dialog-utils";
 
 export const App = () => {
-    const subObj = () => {
-        return (
-            <Fragment>
-                <div>
-                    <br/>
-                    This is a dialog sub-component.
-                    <br/>
-                </div>
-            </Fragment>
-        );
-    };
-
-    const floaterArgs = {title: "The dialog title.", description: "The dialog description.", openDialogImmediately:true, "dialogObject": {...subObj()}};
-
-    return (
+  return (
     <Fragment>
-        <BaseFloatingDialog {...floaterArgs} />
-        <Map />
-     </Fragment>
-    );
+      <Map />
+    </Fragment>
+  );
 };
