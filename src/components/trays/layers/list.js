722f681c670bfb8aa280df69e5d3d285ab36fbe0
Matt Watson <mwatson@renci.org>
2024-05-04 17:25:17 -0400

    break layer card into separate component
    we don't keep expanded cards in state any longer.

--- src/components/trays/layers/list.js
+++ src/components/trays/layers/list.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {
   Accordion,
   AccordionGroup,
@@ -19,35 +20,15 @@ import {
   ArrowDropDown as MoveDownArrow,
   Schedule as ClockIcon,
   DragIndicator as DragHandleIcon,
+  Opacity as OpacityIcon,
+  Palette as ColorRampIcon,
 } from '@mui/icons-material';
 import { useLayers } from '@context';
+import { useToggleState } from '@hooks';
 
 export const LayersList = () => {
-  const {
-    defaultModelLayers,
-    layerTypes,
-    removeLayer,
-    swapLayers,
-    toggleLayerVisibility,
-  } = useLayers();
+  const { defaultModelLayers } = useLayers();
   const layers = [...defaultModelLayers];
-
-  const [expandedIds, setExpandedIds] = useState(new Set());
-
-  const handleToggleExpansion = id => () => {
-    const _expandedIds = new Set([...expandedIds]);
-    if (_expandedIds.has(id)) {
-      _expandedIds.delete(id);
-      setExpandedIds(_expandedIds);
-      return;
-    }
-    _expandedIds.add(id);
-    setExpandedIds(_expandedIds);
-  };
-
-  const handleClickRemove = id => () => {
-    removeLayer(id);
-  };
 
   return (
     <AccordionGroup variant="soft">
@@ -55,137 +36,176 @@ export const LayersList = () => {
         layers
           .sort((a, b) => a.state.order - b.state.order)
           .map((layer, index) => {
-            const isExpanded = expandedIds.has(layer.id);
-            const isVisible = layer.state.visible;
-            const LayerIcon = layerTypes[layer.properties.product_type].icon;
-
             return (
-              <Accordion
-                key={ `layer-${ layer.id }-${ isVisible ? 'visible' : 'hidden' }` }
-                expanded={ isExpanded }
-                onChange={ handleToggleExpansion }
-                sx={{ p: 0 }}
-              >
-                {/*
-                  the usual AccordionSummary component results in a button,
-                  but we want some buttons _inside_ the accordion summary,
-                  so we'll build a custom component here.
-                */}
-                <Stack
-                  direction="row"
-                  gap={ 1 }
-                  sx={{
-                    p: 1,
-                    borderLeft: '6px solid',
-                    borderLeftColor: isVisible ? 'primary.400' : 'primary.100',
-                    '.actions': { filter: 'opacity(0.1)', transition: 'filter 250ms' },
-                    '&:hover .actions': { filter: 'opacity(0.5)' },
-                    '& .actions:hover': { filter: 'opacity(1.0)' },
-                  }}
-                >
-                  <Stack direction="column" sx={{ flex: 1 }}>
-                    <Stack direction="row" alignItems="center" sx={{
-                      flex: 1,
-                      filter: isVisible ? 'opacity(1.0)' : 'opacity(0.75)',
-                      transition: 'filter 250ms',
-                    }}>
-                      <Avatar sx={{ width: 36, height: 36 }}>
-                        <LayerIcon size="lg" color="primary" />
-                      </Avatar>
-                      <Typography level="title-md">
-                        {layerTypes[layer.properties.product_type].name}
-                      </Typography>
-                    </Stack>
-
-                    <Stack direction="row" alignItems="stretch" sx={{ flex: 1 }}>
-                      <IconButton
-                        size="sm"
-                        sx={{
-                          cursor: 'grab',
-                          filter: 'opacity(0.5)',
-                          transition: 'filter 250ms',
-                          '&:hover': {
-                            filter: 'opacity(1.0)',
-                          },
-                          m: 0,
-                        }}
-                      >
-                        <DragHandleIcon fontSize="sm" />
-                      </IconButton>
-                      <Typography level="body-sm" sx={{ display: 'inline-flex', alignItems: 'center' }}>
-                        <ClockIcon sx={{ transform: 'scale(0.66)' }} /> { new Date(layer.properties.run_date).toLocaleString() }
-                        <Typography level="body-xs" sx={{ display: 'inline-flex', alignItems: 'center', ml: 3 }}>
-                          Cycle { layer.properties.cycle }
-                        </Typography>
-                      </Typography>
-                    </Stack>
-                  </Stack>
-
-                  {/* card actions start */}
-                  <Stack justifyContent="space-around" className="actions">
-                    <Switch
-                      size="sm"
-                      checked={ isVisible }
-                      onChange={ () => toggleLayerVisibility(layer.id) }
-                    />
-
-                    <IconButton
-                      onClick={ handleClickRemove(layer.id) }
-                      size="sm"
-                      color="danger"
-                    >
-                      <RemoveIcon />
-                    </IconButton>
-                  </Stack>
-
-                  <ButtonGroup
-                    orientation="vertical"
-                    size="sm"
-                    className="actions"
-                    sx={{
-                      transform: 'scaleX(0.75)',
-                      '.MuiIconButton-root': { flex: 1 }
-                    }}
-                  >
-                    <IconButton
-                      onClick={ () => swapLayers(index, index - 1) }
-                      sx={{ flex: 1 }}
-                    ><MoveUpArrow /></IconButton> 
-                    <IconButton
-                      onClick={ () => swapLayers(index, index + 1) }
-                      sx={{ flex: 1 }}
-                    ><MoveDownArrow /></IconButton> 
-                  </ButtonGroup>
-                  {/* card actions end */}
-                  
-                  <IconButton onClick={ handleToggleExpansion(layer.id) } size="sm" variant="soft">
-                    <ExpandIcon
-                      fontSize="sm"
-                      sx={{
-                        transform: isExpanded ? 'rotate(180deg)' : 'rotate(0)',
-                        transition: 'transform 100ms',
-                      }}
-                    />
-                  </IconButton>
-                </Stack>
-                <AccordionDetails variant="solid" sx={{
-                  // remove default margin that doesn't work well in our situation.
-                  marginInline: 0,
-                }}>
-                  <Box component="pre" sx={{
-                    fontSize: '75%',
-                    color: '#def',
-                    backgroundColor: 'transparent',
-                    overflowX: 'auto',
-                  }}>
-                    { JSON.stringify(layer.properties, null, 2) }
-                  </Box>
-                </AccordionDetails>
-              </Accordion>
+              <LayerCard
+                key={ `layer-${ layer.id }` }
+                layer={ layer }
+                index={ index }
+              />
             );
           })
       }
       <Divider />
     </AccordionGroup>
   );
+};
+
+const LayerCard = ({ index, layer }) => {
+  const {
+    layerTypes,
+    removeLayer,
+    swapLayers,
+    toggleLayerVisibility,
+  } = useLayers();
+  const expanded = useToggleState(false);
+  const isVisible = layer.state.visible;
+  const LayerIcon = layerTypes[layer.properties.product_type].icon;
+
+  const handleClickRemove = id => () => {
+    removeLayer(id);
+  };
+
+  return (
+    <Accordion
+      expanded={ expanded.enabled }
+      onChange={ expanded.toggle }
+      sx={{ p: 0 }}
+    >
+      {/*
+        the usual AccordionSummary component results in a button,
+        but we want some buttons _inside_ the accordion summary,
+        so we'll build a custom component here.
+      */}
+      <Stack
+        direction="row"
+        gap={ 1 }
+        sx={{
+          p: 1,
+          borderLeft: '6px solid',
+          borderLeftColor: isVisible ? 'primary.400' : 'primary.100',
+          '.actions': { filter: 'opacity(0.1)', transition: 'filter 250ms' },
+          '&:hover .actions': { filter: 'opacity(0.5)' },
+          '& .actions:hover': { filter: 'opacity(1.0)' },
+        }}
+      >
+        <Stack direction="column" sx={{ flex: 1 }}>
+          <Stack direction="row" alignItems="center" sx={{
+            flex: 1,
+            filter: isVisible ? 'opacity(1.0)' : 'opacity(0.75)',
+            transition: 'filter 250ms',
+          }}>
+            <Avatar sx={{ width: 36, height: 36 }}>
+              <LayerIcon size="lg" color="primary" />
+            </Avatar>
+            <Typography level="title-md">
+              {layerTypes[layer.properties.product_type].name}
+            </Typography>
+          </Stack>
+
+          <Stack direction="row" alignItems="stretch" sx={{ flex: 1 }}>
+            <IconButton
+              size="sm"
+              sx={{
+                cursor: 'grab',
+                filter: 'opacity(0.5)',
+                transition: 'filter 250ms',
+                '&:hover': {
+                  filter: 'opacity(1.0)',
+                },
+                m: 0,
+              }}
+            >
+              <DragHandleIcon />
+            </IconButton>
+            <Typography level="body-sm" sx={{ display: 'inline-flex', alignItems: 'center' }}>
+              <ClockIcon sx={{ transform: 'scale(0.66)' }} /> { new Date(layer.properties.run_date).toLocaleString() }
+              <Typography level="body-xs" sx={{ display: 'inline-flex', alignItems: 'center', ml: 3 }}>
+                Cycle { layer.properties.cycle }
+              </Typography>
+            </Typography>
+          </Stack>
+        </Stack>
+
+        {/* layer card actions start */}
+        <Stack justifyContent="space-around" className="actions">
+          <Switch
+            size="sm"
+            checked={ isVisible }
+            onChange={ () => toggleLayerVisibility(layer.id) }
+          />
+
+          <IconButton
+            onClick={ handleClickRemove(layer.id) }
+            size="sm"
+            color="danger"
+          >
+            <RemoveIcon />
+          </IconButton>
+        </Stack>
+
+        <Stack justifyContent="space-around" className="actions">
+          <IconButton size="sm" variant="soft" disabled>
+            <OpacityIcon />
+          </IconButton>
+          <IconButton size="sm" variant="soft" disabled>
+            <ColorRampIcon />
+          </IconButton>
+        </Stack>
+
+        <ButtonGroup
+          orientation="vertical"
+          size="sm"
+          className="actions"
+          sx={{
+            transform: 'scaleX(0.75)',
+            '.MuiIconButton-root': { flex: 1 }
+          }}
+        >
+          <IconButton
+            onClick={ () => swapLayers(index, index - 1) }
+            sx={{ flex: 1 }}
+          ><MoveUpArrow /></IconButton> 
+          <IconButton
+            onClick={ () => swapLayers(index, index + 1) }
+            sx={{ flex: 1 }}
+          ><MoveDownArrow /></IconButton> 
+        </ButtonGroup>
+
+        {/* layer card actions end */}
+        
+        <IconButton
+          onClick={ expanded.toggle }
+          size="sm"
+          variant="soft"
+        >
+          <ExpandIcon
+            fontSize="sm"
+            sx={{
+              transform: expanded.enabled ? 'rotate(180deg)' : 'rotate(0)',
+              transition: 'transform 100ms',
+            }}
+          />
+        </IconButton>
+      </Stack>
+      <AccordionDetails variant="solid" sx={{
+        // remove default margin that doesn't work well in our situation.
+        marginInline: 0,
+      }}>
+        <Box component="pre" sx={{
+          fontSize: '75%',
+          color: '#def',
+          backgroundColor: 'transparent',
+          overflowX: 'auto',
+        }}>
+          { JSON.stringify(layer.properties, null, 2) }
+        </Box>
+      </AccordionDetails>
+    </Accordion>
+
+  );
+};
+
+LayerCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  layer: PropTypes.object.isRequired,
 };
