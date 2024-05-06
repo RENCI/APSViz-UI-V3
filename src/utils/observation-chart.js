387e3d42766acaf239a91df9995cf21477c1ce60
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-05-06 12:15:58 -0400

    checkpointing current state of code

--- src/utils/observation-chart.js
+++ src/utils/observation-chart.js
@@ -1,5 +1,5 @@
 import React, {Fragment, useState, useEffect} from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 import PropTypes from 'prop-types';
 
@@ -45,54 +45,9 @@ function csvToJSON(csvData) {
         }
 
         // TODO: return the data as a json string (for now)
-        return JSON.stringify(ret_val); // Returns JSON string
+        return ret_val;
     }
 }
-
-const data = [
-  {
-    name: 'Page A',
-    uv: 4000,
-    pv: 2400,
-    amt: 2400,
-  },
-  {
-    name: 'Page B',
-    uv: 3000,
-    pv: 1398,
-    amt: 2210,
-  },
-  {
-    name: 'Page C',
-    uv: 2000,
-    pv: 9800,
-    amt: 2290,
-  },
-  {
-    name: 'Page D',
-    uv: 2780,
-    pv: 3908,
-    amt: 2000,
-  },
-  {
-    name: 'Page E',
-    uv: 1890,
-    pv: 4800,
-    amt: 2181,
-  },
-  {
-    name: 'Page F',
-    uv: 2390,
-    pv: 3800,
-    amt: 2500,
-  },
-  {
-    name: 'Page G',
-    uv: 3490,
-    pv: 4300,
-    amt: 2100,
-  },
-];
 
 /**
  * renders the observations as a chart
@@ -127,21 +82,24 @@ export default function ObservationChart(dataUrl) {
     // render the chart.
     return (
         <Fragment>
-            {/*{stationObs}*/}
-            {/*<ResponsiveContainer width="100%" height="100%">*/}
-            {/*    <LineChart*/}
-            {/*        width={500}*/}
-            {/*        height={300}*/}
-            {/*        data={data}*/}
-            {/*        margin={{*/}
-            {/*            top: 5,*/}
-            {/*            right: 30,*/}
-            {/*            left: 20,*/}
-            {/*            bottom: 5,*/}
-            {/*        }}*/}
-            {/*    >*/}
-            {/*    </LineChart>*/}
-            {/*</ResponsiveContainer>*/}
-        </Fragment>
+        <LineChart
+          width={500}
+          height={300}
+          data={stationObs}
+          margin={{
+            top: 0,
+            right: 0,
+            left: 0,
+            bottom: 0,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="time" />
+          <YAxis type="number" domain={['auto', 'auto']} />
+          <Tooltip />
+          <Legend />
+          <Line type="monotone" dataKey="Observations" stroke="#8884d8" strokeWidth={2} dot={false} isAnimationActive={false} />
+          <Line type="monotone" dataKey="NOAA Tidal Predictions" stroke="#82ca9d" strokeWidth={2} dot={false} isAnimationActive={false} />
+        </LineChart></Fragment>
     );
 };
