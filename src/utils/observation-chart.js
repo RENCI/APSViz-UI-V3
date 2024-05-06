7bf94d080dafb83060bb1be98b8cd96e5e5f951c
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-05-06 09:28:52 -0400

    checkpointing current state of branch

--- src/utils/observation-chart.js
+++ src/utils/observation-chart.js
@@ -1,4 +1,6 @@
 import React, {Fragment, useState, useEffect} from 'react';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 import PropTypes from 'prop-types';
 
 // define the properties of this component
@@ -47,6 +49,51 @@ function csvToJSON(csvData) {
     }
 }
 
+const data = [
+  {
+    name: 'Page A',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'Page B',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'Page C',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'Page D',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'Page E',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'Page F',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'Page G',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+];
+
 /**
  * renders the observations as a chart
  *
@@ -55,7 +102,7 @@ function csvToJSON(csvData) {
  * @constructor
  */
 export default function ObservationChart(dataUrl) {
-    // store the items in state
+    // store the station observation data in state
     const [stationObs, setStationObs] = useState("");
 
     // get the data
@@ -67,7 +114,7 @@ export default function ObservationChart(dataUrl) {
                 })
                 .then(data => {
                     setStationObs(csvToJSON(data));
-                    console.log(JSON.stringify(data));
+                    // console.log(JSON.stringify(data));
                 })
                 .catch(err => {
                     console.log(err);
@@ -80,7 +127,21 @@ export default function ObservationChart(dataUrl) {
     // render the chart.
     return (
         <Fragment>
-            {stationObs}
+            {/*{stationObs}*/}
+            {/*<ResponsiveContainer width="100%" height="100%">*/}
+            {/*    <LineChart*/}
+            {/*        width={500}*/}
+            {/*        height={300}*/}
+            {/*        data={data}*/}
+            {/*        margin={{*/}
+            {/*            top: 5,*/}
+            {/*            right: 30,*/}
+            {/*            left: 20,*/}
+            {/*            bottom: 5,*/}
+            {/*        }}*/}
+            {/*    >*/}
+            {/*    </LineChart>*/}
+            {/*</ResponsiveContainer>*/}
         </Fragment>
     );
 };
