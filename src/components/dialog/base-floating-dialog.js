bd530232007a92f3d1144c989b3dfb62c25a2385
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-05-07 11:25:14 -0400

    trying to resync with main

--- src/components/dialog/base-floating-dialog.js
+++ src/components/dialog/base-floating-dialog.js
@@ -63,12 +63,12 @@ export default function BaseFloatingDialog({ title, dialogObject, dataKey, dataL
                     TransitionComponent={Transition}
                     disableEnforceFocus
                     style={{ pointerEvents: 'none' }}
-                    PaperProps={{ sx: { width: 750,  height: 510, pointerEvents: 'auto'} }}
-                    sx={{ width: 750, height: 510, '.MuiBackdrop-root': { backgroundColor: 'transparent' }}}
+                    PaperProps={{ sx: { width: 625,  height: 510, pointerEvents: 'auto'} }}
+                    sx={{ width: 625, height: 510, '.MuiBackdrop-root': { backgroundColor: 'transparent' }}}
                 >
                     <DialogTitle sx={{cursor: 'move', backgroundColor: 'lightblue', textAlign: 'center', fontSize: 14, height: 35, m: 0, p: 1 }} id="draggable-dialog-title"> { title } </DialogTitle>
 
-                    <DialogContent sx={{backgroundColor: 'white', fontSize: 14, height: 375 }}>{ dialogObject }</DialogContent>
+                    <DialogContent sx={{backgroundColor: 'white', fontSize: 14, m: 0, width: 500, height: 375 }}>{ dialogObject }</DialogContent>
 
                     <DialogActions sx={{backgroundColor: 'lightgray', height: 35, m: 0, p: 1}}><Button style={{fontSize: 14}} autoFocus onClick={handleClose}> Close </Button></DialogActions>
                 </Dialog>
--- src/components/map/map.js
+++ src/components/map/map.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import { DefaultLayers } from './default-layers';
-import { useLayers } from '@context';
+import {
+  useLayers,
+  useSettings,
+} from '@context';
 import 'leaflet/dist/leaflet.css';
 
 const DEFAULT_CENTER = [30.0, -73.0];
 
 export const Map = () => {
+    const { darkMode } = useSettings();
     const {
       setMap
     } = useLayers();
@@ -17,12 +21,12 @@ export const Map = () => {
         zoom={5}
         zoomControl={false}
         scrollWheelZoom={true}
-        ref={setMap}
+        whenCreated={setMap}
         style={{ height: '100vh', width:'100wh' }}>
-          <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
+          { darkMode.enabled
+            ? <TileLayer url={ `https://api.mapbox.com/styles/v1/mvvatson/clvu3inqs05v901qlabcfhxsr/tiles/256/{z}/{x}/{y}@2x?access_token=${ process.env.REACT_APP_MAPBOX_TOKEN }` } />
+            : <TileLayer url={ `https://api.mapbox.com/styles/v1/mvvatson/clvu2u7iu061901ph15n55v2e/tiles/256/{z}/{x}/{y}@2x?access_token=${ process.env.REACT_APP_MAPBOX_TOKEN }` } />
+          }
           <DefaultLayers/>
       </MapContainer>
     );
