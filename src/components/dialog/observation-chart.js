859f81eecb8728da4214491db3031e3fe4649628
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-05-08 12:06:16 -0400

    correcting the look of the graph

--- src/components/dialog/observation-chart.js
+++ src/components/dialog/observation-chart.js
@@ -44,6 +44,15 @@ function csvToJSON(csvData) {
             ret_val.push(jsonObj);
         }
 
+        // remove the timezone from the time value
+        ret_val.map(function (e){
+            e.time = e.time.substring(0, e.time.split(':', 2).join(':').length);
+            if (e["APS Nowcast"]) e["APS Nowcast"] = +parseFloat(e["APS Nowcast"]).toFixed(4);
+            if (e["Observations"]) e["Observations"] = +parseFloat(e["Observations"]).toFixed(4);
+            if (e["NOAA Tidal Predictions"]) e["NOAA Tidal Predictions"] = +parseFloat(e["NOAA Tidal Predictions"]).toFixed(3);
+            if (e["Difference (APS-OBS)"]) e["Difference (APS-OBS)"] = +parseFloat(e["Difference (APS-OBS)"]).toFixed(3);
+        });
+
         // return the json data representation
         return ret_val;
     }
@@ -83,12 +92,12 @@ export default function ObservationChart(dataUrl) {
     // render the chart.
     return (
         <Fragment>
-            <LineChart width={550} height={300} data={stationObs} margin={{  top: 0, right: 0, left: 0, bottom: 0 }}>
+            <LineChart width={590} height={300} data={stationObs} margin={{ top: 0, right: 0, left: 0, bottom: 0 }}>
                 <CartesianGrid strokeDasharray="3 3" />
                 <XAxis dataKey="time" />
-                <YAxis />
+                <YAxis domain={['dataMin', 'dataMax']} />
                 <Tooltip />
-                <Legend verticalAlign="top" height={20} />
+                <Legend verticalAlign="bottom" height={30} />
                 <Line type="monotone" dataKey="Observations" stroke="gray" strokeWidth={2} dot={false} isAnimationActive={false} />
                 <Line type="monotone" dataKey="NOAA Tidal Predictions" stroke="teal" strokeWidth={2} dot={false} isAnimationActive={false} />
                 <Line type="monotone" dataKey="APS Nowcast" stroke="CornflowerBlue" strokeWidth={2} dot={false} isAnimationActive={false} />
