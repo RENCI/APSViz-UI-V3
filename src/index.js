94ecc70d43c0b206666a91720b692b6650938e2f
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-05-08 08:55:59 -0400

    making it so that mui material and mui joy are symbiotic

--- src/index.js
+++ src/index.js
@@ -1,46 +1,30 @@
-// import React from 'react';
-// import { CssVarsProvider } from '@mui/joy/styles';
-// import { createRoot } from 'react-dom/client';
-// import { App } from './app';
-// import { LayersProvider, SettingsProvider } from '@context';
-// import './index.css';
-// import '@fontsource/inter';
-// import theme from './theme';
-//
-// const container = document.getElementById('root');
-// const root = createRoot(container);
-//
-// const ProvisionedApp = () => (
-//   <CssVarsProvider theme={ theme } defaultMode="system">
-//     <SettingsProvider>
-//       <LayersProvider>
-//         <App />
-//       </LayersProvider>
-//     </SettingsProvider>
-//   </CssVarsProvider>
-// );
-
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
 import { LayersProvider, SettingsProvider } from '@context';
+import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import {
+    experimental_extendTheme as materialExtendTheme,
+    Experimental_CssVarsProvider as MaterialCssVarsProvider,
+    THEME_ID as MATERIAL_THEME_ID
+} from '@mui/material/styles';
+
 import './index.css';
 import '@fontsource/inter';
 import theme from './theme';
 
-import {
-  experimental_extendTheme as materialExtendTheme,
-  Experimental_CssVarsProvider as MaterialCssVarsProvider
-  ,THEME_ID as MATERIAL_THEME_ID,
-} from '@mui/material/styles';
-import { CssVarsProvider as JoyCssVarsProvider } from '@mui/joy/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-
+// get a reference to the root element used as a rendering target
 const container = document.getElementById('root');
+
+// create the root container
 const root = createRoot(container);
 
+// create a new material theme. this is so it can be differentiated from the joy theme
 const materialTheme = materialExtendTheme();
 
+// render the app specifying the material and joy providers
 const ProvisionedApp = () => {
     return (
         <MaterialCssVarsProvider theme={{ [MATERIAL_THEME_ID]: materialTheme }}>
@@ -56,4 +40,5 @@ const ProvisionedApp = () => {
     );
 };
 
+// render the app
 root.render(<ProvisionedApp />);
