ea5ea5adee20377ac080ee2eb16f5b9c5332515c
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-05-09 11:35:42 -0400

    adding form placeholders

--- src/components/layer-selection/layerSelectionForm.js
+++ src/components/layer-selection/layerSelectionForm.js
@@ -31,16 +31,26 @@ export const LayerSelectionForm = () => {
                 <TabPanel value={0}>
                     <form onSubmit = { formHandler }>
                         <Stack spacing={1}>
-                            <Input name="tropical" placeholder="Tropical tab control" required />
+                            <Input name="tropical-storm-name" placeholder="Tropical storm name control" />
+                            <Input name="tropical-advisory" placeholder="Tropical advisory control" />
+                            <Input name="tropical-grid" placeholder="Tropical grid control" />
+                            <Input name="tropical-instance" placeholder="Tropical instance control" />
+
                             <Button type="submit">Submit</Button>
+                            <Button type="reset">Reset</Button>
                         </Stack>
                     </form>
                 </TabPanel>
                 <TabPanel value={1}>
                     <form onSubmit={ formHandler }>
                         <Stack spacing={1}>
-                            <Input name="synoptic" placeholder="Synoptic tab control" required />
+                            <Input name="synoptic-date" placeholder="Synoptic date control" />
+                            <Input name="synoptic-cycle" placeholder="Synoptic cycle control" />
+                            <Input name="synoptic-grid" placeholder="Synoptic grid control" />
+                            <Input name="synoptic-instance" placeholder="Synoptic instance control" />
+
                             <Button type="submit">Submit</Button>
+                            <Button type="reset">Reset</Button>
                         </Stack>
                     </form>
                 </TabPanel>
