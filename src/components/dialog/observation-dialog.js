d320fe35212a9b5a132cd83667cef678a6011db4
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-05-16 16:44:37 -0400

    tidying up

--- src/components/dialog/observation-dialog.js
+++ src/components/dialog/observation-dialog.js
@@ -28,8 +28,15 @@ export const ObservationDialog = (obs_data) => {
         );
     };
 
-    // create an object for the base dialog
-    const floaterArgs = {title: obs_data.obs.location_name, dialogObject: {...graphObj(obs_data.obs.csvurl)}, dataKey: obs_data.obs.station_name, dataList: selectedObservations, setDataList: setSelectedObservations, map: map};
+    // create a data object for the base dialog to use to render
+    const floaterArgs = {
+        title: obs_data.obs.location_name,
+        dialogObject: {...graphObj(obs_data.obs.csvurl)},
+        dataKey: obs_data.obs.station_name,
+        dataList: selectedObservations,
+        setDataList: setSelectedObservations,
+        map: map
+    };
 
     // render the dialog
     return (
