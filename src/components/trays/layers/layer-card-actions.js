eb8edf3b35a007c4487144d4113e2dc40d7ab6b1
Matt Watson <mwatson@renci.org>
2024-05-20 20:18:54 -0400

    set up layer action tabs
    tidy. remove obsolete component
    
    newlines

--- src/components/trays/layers/layer-card-actions.js
+++ src/components/trays/layers/layer-card-actions.js
@@ -1,54 +1,79 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
+  Box,
   Divider,
   Tab,
   TabList,
   Tabs,
   TabPanel,
   Stack,
+  Typography,
 } from '@mui/joy';
 import {
   DeleteForever as RemoveIcon,
-  Opacity as OpacityIcon,
   Palette as ColorRampIcon,
   DataObject as RawDataIcon,
 } from '@mui/icons-material';
 import { useLayers } from '@context';
 import { ActionButton } from '@components/buttons';
 
-export const LayerActions = ({ layerId = 0 }) => {
+export const LayerActions = ({ layer }) => {
   const { removeLayer } = useLayers();
 
   return (
-    <Stack
-      direction="row"
-      justifyContent="space-between"
-    >
-      <Tabs sx={{ flex: 1 }}>
-        <TabList>
+    <Tabs defaultValue={0}>
+      <Stack
+        direction="row"
+        justifyContent="space-between"
+      >
+        <TabList sx={{ flex: 1 }}>
           <Tab><RawDataIcon /></Tab>
-          <Tab><OpacityIcon /></Tab>
           <Tab><ColorRampIcon /></Tab>
         </TabList>
-      </Tabs>
 
-      <Divider orientation="vertical" />
+        <Divider orientation="vertical" />
 
-      <ActionButton
-        color="warning"
-        onClick={ () => removeLayer(layerId) }
-        sx={{
-          borderRadius: 0,
-          borderBottom: '1px solid var(--joy-palette-divider)',
-        }}
-      >
-        <RemoveIcon />
-      </ActionButton>
-    </Stack>
+        <ActionButton
+          color="warning"
+          onClick={ () => removeLayer(layer.id) }
+          sx={{
+            borderRadius: 0,
+            borderBottom: '1px solid var(--joy-palette-divider)',
+          }}
+        >
+          <RemoveIcon />
+        </ActionButton>
+      </Stack>
+
+      <TabPanel value={ 0 }>
+        <Typography
+          level="title-sm"
+          textTransform="uppercase"
+        >Metadata</Typography>
+        <Box component="pre" sx={{
+          fontSize: '75%',
+          color: 'text.primary',
+          backgroundColor: 'transparent',
+          overflowX: 'auto',
+          m: 0, p: 1,
+        }}>
+          { JSON.stringify(layer.properties, null, 2) }
+        </Box>
+      </TabPanel>
+      
+      <TabPanel value={ 1 }>
+        <Typography
+          level="title-sm"
+          textTransform="uppercase"
+        >Appearance</Typography>
+        - opacity <br />
+        - color ramp selection <br />
+      </TabPanel>
+    </Tabs>
   );
 };
 
 LayerActions.propTypes = {
-  layerId: PropTypes.string.isRequired,
+  layer: PropTypes.object.isRequired,
 };
