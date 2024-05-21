6e1cce7dca10dbb28243c670e5f03a4ea8e3e6c3
Matt Watson <mwatson@renci.org>
2024-05-20 20:06:55 -0400

    flesh out action selection menu

--- src/components/trays/layers/layer-card-actions.js
+++ src/components/trays/layers/layer-card-actions.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Divider } from '@mui/joy';
+import {
+  Divider,
+  Tab,
+  TabList,
+  Tabs,
+  TabPanel,
+  Stack,
+} from '@mui/joy';
 import {
   DeleteForever as RemoveIcon,
   Opacity as OpacityIcon,
@@ -9,35 +16,36 @@ import {
 } from '@mui/icons-material';
 import { useLayers } from '@context';
 import { ActionButton } from '@components/buttons';
-import { ActionButtonMenu } from '@components/buttons';
 
 export const LayerActions = ({ layerId = 0 }) => {
   const { removeLayer } = useLayers();
 
   return (
-    <ActionButtonMenu>
-      <ActionButton disabled>
-        <RawDataIcon />
-      </ActionButton>
-
-      <ActionButton disabled>
-        <OpacityIcon />
-      </ActionButton>
-
-      <ActionButton disabled>
-        <ColorRampIcon />
-      </ActionButton>
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+    >
+      <Tabs sx={{ flex: 1 }}>
+        <TabList>
+          <Tab><RawDataIcon /></Tab>
+          <Tab><OpacityIcon /></Tab>
+          <Tab><ColorRampIcon /></Tab>
+        </TabList>
+      </Tabs>
 
       <Divider orientation="vertical" />
 
       <ActionButton
-        variant="solid"
         color="warning"
         onClick={ () => removeLayer(layerId) }
+        sx={{
+          borderRadius: 0,
+          borderBottom: '1px solid var(--joy-palette-divider)',
+        }}
       >
         <RemoveIcon />
       </ActionButton>
-    </ActionButtonMenu>
+    </Stack>
   );
 };
 
