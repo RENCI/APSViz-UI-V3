76a3ab561800d6d82aa5697031448235f79118e8
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-05-22 13:05:27 -0400

    adding additional error checking

--- src/components/model-selection/DropDownOptions.js
+++ src/components/model-selection/DropDownOptions.js
@@ -14,14 +14,32 @@ export default function DropDownOptions(data) {
     DropDownOptions.propTypes = { data: PropTypes.any };
 
     // do not render if there is no data
-    if (data.data == null) return;
-
-    // return all the options
-    return (
-        <Fragment>
-            {data.data[data.type].filter(item => item !== "").map(item => (
-                <Option key={item} value={item}>{item}</Option>
-            ))}
-        </Fragment>
-    );
+    if (data.data != null) {
+        // if there is a warning getting the result
+        if (data.data['Warning'] !== undefined) {
+            return (
+                <div>
+                    Warning: {data.data['Warning']}
+                </div>
+            );
+        }
+        // if there is an error getting the result
+        else if(data.data['Error'] !== undefined) {
+            return (
+                <div>
+                    Error: {data.data['Error']}
+                </div>
+            );
+        }
+        // return all the options
+        else {
+            return (
+                <Fragment>
+                    {data.data[data.type].filter(item => item !== "").map(item => (
+                        <Option key={item} value={item}>{item}</Option>
+                    ))}
+                </Fragment>
+            );
+        }
+    }
 }
