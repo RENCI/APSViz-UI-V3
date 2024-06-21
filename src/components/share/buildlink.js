57a437967d0b791ebd5e19c468ec1cf2aad27dd9
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-06-21 08:58:16 -0400

    adding "comments" capture in a pop-up dialog.

--- src/components/share/buildlink.js
+++ src/components/share/buildlink.js
@@ -1,7 +1,8 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import { Button, TextField, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
 import { IconButton } from '@mui/joy';
 import ShareRoundedIcon from '@mui/icons-material/ShareRounded';
-import {useLayers} from "@context/map-context";
+import { useLayers } from "@context/map-context";
 
 /**
  * renders the link builder to recreate the current view elsewhere
@@ -10,13 +11,16 @@ import {useLayers} from "@context/map-context";
  * @constructor
  */
 export const BuildLink = () => {
-    // get the layers in state
+    // get the layers from state
     const { defaultModelLayers } = useLayers();
+
+    // used to set the dialog view state
+    const [open, setOpen] = useState(false);
 
     /**
      * create the query string that can be used to share the current view
      */
-    const createLink = () => {
+    const createLink = (comment) => {
         // get the list of selected layers
         const groups = defaultModelLayers
             // get all the distinct groups
@@ -26,16 +30,13 @@ export const BuildLink = () => {
             .map((mbr) => (
                 mbr['group']
             ))
-            // generate a query string
-            .join(',');
+            // generate a run id
+            .join(',').split(',')[0];
 
         // check to see if there was one or more groups selected
         if (groups !== '') {
             // copy the link to the cut/paste buffer
-            copyTextToClipboard(window.location.origin + '/#share=' + groups).then();
-
-            // tell the user what just happened
-            alert('The share link has been copied to the clipboard.');
+            copyTextToClipboard(encodeURI(window.location.origin + '/#share=' + groups + ',' + comment)).then();
         }
         // no layers were selected on the map
         else
@@ -53,12 +54,47 @@ export const BuildLink = () => {
         return await navigator.clipboard.writeText(text);
     }
 
-    // render the button
+    // handles the dialog open event
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    // handles the dialog close event
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
         <Fragment>
-            <IconButton onClick={ createLink }>Share&nbsp;
+            <IconButton onClick={handleClickOpen}>Share&nbsp;
                 <ShareRoundedIcon color={'primary'} />
             </IconButton>
+            <Dialog
+                open={ open }
+                onClose={ handleClose }
+                PaperProps={{
+                    component: 'form',
+                    onSubmit: (event) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries(formData.entries());
+                        const comment = formJson.comment;
+                        createLink(comment);
+                        handleClose();
+                    }}}>
+                <DialogTitle>Share this view</DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Please enter a comment below that describes what you are sharing. When ready, select the &quot;Create&quot; button
+                        and you will have the share link copied into your cut/paste buffer.
+                    </DialogContentText>
+                    <TextField autoFocus margin="dense" id="comment" name="comment" fullWidth variant="standard"/>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Cancel</Button>
+                    <Button type="submit">Create</Button>
+                </DialogActions>
+            </Dialog>
         </Fragment>
     );
 };
