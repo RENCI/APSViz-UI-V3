dc7956c1e3cb5fbf046253eeb2d4b456291d81fc
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-06-24 12:31:50 -0400

    tidying up

--- src/components/map/default-layers.js
+++ src/components/map/default-layers.js
@@ -71,7 +71,6 @@ export const DefaultLayers = () => {
           layer.on("mouseout", function () {
             this.closePopup();
           });
-
           layer.on("click", function (e) {
             // this id is used to remove a selected observation from the selectedObservations list when the dialog is closed
             feature.properties.id = feature.properties.station_name;
@@ -145,6 +144,8 @@ export const DefaultLayers = () => {
                                 "&typeName=" +
                                 obsLayer.layers;
                 const {data} = await axios.get(obs_url);
+
+                // save the observation data
                 setObsData(data);
 
                 // update the selected observations specified on the share link
