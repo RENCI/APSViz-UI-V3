90b8383fab23a2bd48a24afba3e07a2d50a69f0c
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-08-01 14:30:41 -0400

    implementing drag/drop of the model runs, tidying up code layout.

--- src/components/trays/layers/list.js
+++ src/components/trays/layers/list.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import {
-    AccordionGroup, Box,
-    Divider, Accordion, AccordionSummary, AccordionDetails
-} from '@mui/joy';
-import {useLayers} from '@context';
-import {LayerCard} from './layer-card';
-import {DeleteModelRunButton} from "@components/trays/layers/delete-layer-button";
-import Draggable from 'react-draggable';
+import { AccordionGroup, Box,  Divider, Accordion, AccordionSummary, AccordionDetails, Stack } from '@mui/joy';
+import { useLayers } from '@context';
+import { LayerCard } from './layer-card';
+import { DeleteModelRunButton } from "@components/trays/layers/delete-layer-button";
+import { DragHandleRounded as Handle } from '@mui/icons-material';
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
 /**
  * gets the header data property name index
@@ -16,13 +14,14 @@ import Draggable from 'react-draggable';
  * @param type
  * @returns {string}
  */
-const getPropertyName = (layerProps, type) => {
+const getPropertyName = (layerProps, element_name) => {
     // init the return
     let ret_val = undefined;
 
     // capture the name of the element for tropical storms and advisory numbers
     if (layerProps['met_class'] === 'tropical') {
-        switch (type) {
+        // by the element name
+        switch (element_name) {
             case 'stormOrModelEle':
                 ret_val = layerProps['storm_name'];
                 break;
@@ -36,7 +35,7 @@ const getPropertyName = (layerProps, type) => {
     }
     // capture the name of the synoptic ADCIRC models and cycle numbers
     else {
-        switch (type) {
+        switch (element_name) {
             case 'stormOrModelEle':
                 ret_val = layerProps['model'];
                 break;
@@ -110,8 +109,6 @@ const getGroupList = (layers) => {
         // filter by the group name
         .filter((groups, idx, self) =>
             (idx === self.findIndex((t) => (t['group'] === groups['group']))))
-        // .sort((a, b) =>
-        //     a['run_date'] < b['run_date'] ? 1 : -1)
         // at this point we have the distinct runs
         .map((layer) => {
             groupList.push(layer);
@@ -137,39 +134,67 @@ export const LayersList = () => {
     // get the unique groups in the selected run groups
     const groupList = getGroupList(layers);
 
-    const nodeRef = React.useRef(null);
+    // handle the drag event
+    const onDragEnd = (result) => {
+        // dropped outside the list
+        if (!result.destination) {
+          return;
+        }
+
+        // reorder the layer list
+        reorder(
+          layers,
+          result.source.index,
+          result.destination.index
+        );
+    };
+
+    // a little function to help us with reordering the result
+    const reorder = (list, startIndex, endIndex) => {
+        const result = Array.from(list);
+        const [removed] = result.splice(startIndex, 1);
+        result.splice(endIndex, 0, removed);
+
+        return result;
+    };
 
     // loop through the layers and put them away
     return (
-        <Draggable nodeRef={nodeRef} handle="#draggable-dialog">
-            <AccordionGroup variant="soft">
-                {
-                    // loop through the layer groups and put them away
-                    groupList
-                        // filter by the group name
-                        .filter((groups, idx, self) =>
-                            (idx === self.findIndex((t) => (t['group'] === groups['group']))))
-                        // at this point we have the distinct runs
-                        .map((groups, idx) => {
-                            return (
-                                <Accordion key={idx}>
-                                    <Box sx={{display: "flex"}}>
-                                        <DeleteModelRunButton groupId={groups['group']}/>
-                                        <Box>
-                                            <AccordionSummary sx={{fontSize: 12}}>{getHeaderSummary(groups['properties'])} </AccordionSummary>
-                                        </Box>
-                                    </Box>
+        <DragDropContext onDragEnd={ onDragEnd }>
+            <Droppable droppableId="model-runs">
+                { (provided) => (
+                    <AccordionGroup variant="soft" { ...provided['droppableProps'] } ref={ provided['innerRef'] }>
+                        {
+                            // loop through the layer groups and put them away
+                            groupList
+                                // filter by the group name
+                                .filter((group, idx, self) =>
+                                    (idx === self.findIndex((t) => (t['group'] === group['group']))))
+                                // at this point we have the distinct runs
+                                .map((group, idx) => (
+                                    <Draggable key={ group.id } draggableId={ group.id } index={ idx }>
+                                        {(provided) => (
+                                            <Box ref={ provided['innerRef'] } { ...provided['draggableProps'] } { ...provided['dragHandleProps'] }>
+                                                <Accordion>
+                                                    <Box sx={{display: "flex"}}>
+                                                        <Stack direction="row" justifyContent="space-between">
+                                                            <Box>
+                                                                <AccordionSummary sx={{fontSize: 12}}>{ getHeaderSummary(group['properties']) } </AccordionSummary>
+                                                            </Box>
+                                                            <DeleteModelRunButton groupId={ group['group'] }/>
+                                                            <Handle sx={{ fontSize: 25, marginTop: 1.5 }}/>
+                                                        </Stack>
+                                                    </Box>
 
-                                    <AccordionDetails>
-                                        {renderLayerCards(layers, groups['group'])}
-                                    </AccordionDetails>
-                                </Accordion>
-                            );
-                        })
-                }
-                <Divider/>
-            </AccordionGroup>
-        </Draggable>
+                                                    <AccordionDetails> { renderLayerCards(layers, group['group'] )} </AccordionDetails>
+                                                </Accordion>
+                                                <Divider/>
+                                            </Box> )}
+                                    </Draggable> ))
+                        } { provided.placeholder }
+                    </AccordionGroup> )}
+            </Droppable>
+        </DragDropContext>
     );
 };
 
