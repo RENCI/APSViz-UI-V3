3f0fbf189c120b4c48bc9d8ab414a7824db88d5d
Lisa Stillwell <lisa@renci.org>
2024-08-13 13:42:16 -0400

    merging main

---
+++ src/components/map/adcirc-raster-layer.js
@@ -0,0 +1,72 @@
+import React, { Fragment, useEffect, useMemo, useState } from 'react';
+import { WMSTileLayer } from 'react-leaflet';
+import { useLocalStorage } from '@hooks';
+import SldStyleParser from 'geostyler-sld-parser';
+import { getNamespacedEnvParam } from '@utils/map-utils';
+
+export const AdcircRasterLayer = (layer, opacity) => {
+
+    const sldParser = new SldStyleParser();
+    const gs_wfs_url = `${ getNamespacedEnvParam('REACT_APP_GS_DATA_URL') }`;
+    const gs_wms_url = gs_wfs_url + 'wms';
+
+    const [storedMaxeleStyle, setStoredMaxeleStyle] = useLocalStorage('maxele', '');
+    const [storedMaxwvelStyle, setStoredMaxwvelStyle] = useLocalStorage('maxwvel', '');
+    const [storedSwanStyle, setStoredSwanStyle] = useLocalStorage('swan', '');
+    const [currentStyle, setCurrentStyle] = useState("");
+
+   useEffect(() => {
+        if(layer.layer.properties) {
+            let style = "";
+            switch(layer.layer.properties.product_type) {
+            case ("maxwvel"):
+                style = storedMaxwvelStyle;
+                break;
+            case ("swan_HS_max63"):
+                style = storedSwanStyle;
+                break;
+            default:
+                style = storedMaxeleStyle;
+                break;
+            }
+
+            sldParser
+            .readStyle(style)
+            .then((geostylerStyle) => {
+                geostylerStyle.output.name = (' ' + layer.layer.layers).slice(1);
+
+                sldParser.writeStyle(geostylerStyle.output)
+                .then((sldStyle) => {
+                    //currentStyle = sldStyle.output.slice(0);
+                    setCurrentStyle(sldStyle.output);
+            });
+            }); 
+        }
+      }, []);
+
+
+    /* const wmsLayerParams = useMemo(() => ({
+        format:"image/png",
+        transparent: true,
+        sld_body: currentStyle,
+    }), []); */
+    const wmsLayerParams = {
+        format:"image/png",
+        transparent: true,
+        sld_body: currentStyle,
+    };
+
+    return (
+        (currentStyle &&
+        <Fragment>
+            (<WMSTileLayer
+                url={gs_wms_url}
+                layers={layer.layer.layers}
+                params={wmsLayerParams}
+                opacity={opacity}
+            />)
+         </Fragment>
+        )
+     );
+
+};
