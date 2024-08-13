4085b14f4de90b0db5b4d03b45a92bda01a23cf1
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-08-13 16:24:18 -0400

    looking into the date picker validation

--- src/components/trays/model-selection/synopticTab.js
+++ src/components/trays/model-selection/synopticTab.js
@@ -25,13 +25,29 @@ export const SynopticTabForm = () => {
 
     // init the data urls
     const rootUrl = `${ getNamespacedEnvParam('REACT_APP_UI_DATA_URL') }`;
-    const basePulldownUrl = `get_pulldown_data?met_class=synoptic&use_v3_sp=true${ getBrandingHandler() }`;
+    const basePulldownUrl = `get_pulldown_data?met_class=synoptic${ getBrandingHandler() }`;
     const baseDataUrl = `get_ui_data_secure?limit=14&met_class=synoptic&use_v3_sp=true${ getBrandingHandler() }`;
     const [finalDataUrl, setFinalDataUrl] = useState(rootUrl + basePulldownUrl);
 
     // storage for received data to render pulldowns
     const [dropDownData, setDropDownData] = useState(null);
     const [catalogData, setCatalogData] = useState(null);
+
+    const setChangedSynopticDate = (newValue) => {
+        // if there was a valid date
+        if (!isNaN(newValue) && newValue !== null) {
+            console.log('Good date: ' + newValue.$d);
+            // save the date
+            setSynopticDate(newValue.$d);
+        }
+        // else handle a bad date
+        else {
+            console.log('Bad date: ' + newValue);
+            // clear the date
+            // setSynopticDate(null);
+            setError('Invalid Date.');
+        }
+    };
 
     /**
      * method to initiate a model search with the filter selections on the synoptic form
@@ -79,27 +95,43 @@ export const SynopticTabForm = () => {
             };
 
             // make the call to get the data
-            const {data} = await axios.get(finalDataUrl, requestOptions);
+            const ret_val = await axios
+                // make the call to get the data
+                .get(finalDataUrl, requestOptions)
+                // use the data returned
+                .then (( response ) => {
+                    // return the data
+                    return response.data;
+                })
+                .catch (( error ) => {
+                    // make sure we do not render anything
+                    return error.response.status;
+                });
 
-            // check the request type
-            if (finalDataUrl.indexOf('get_pulldown_data') !== -1) {
-                // save the dropdown data
-                setDropDownData(data);
+            // if there was an error from the web service
+            if (ret_val !== 500) {
+                // check the request type to save it to the correct place
+                if (finalDataUrl.indexOf('get_pulldown_data') !== -1) {
+                    console.log('finalDataUrl: ' + finalDataUrl);
+
+                    // save the dropdown data
+                    setDropDownData(ret_val);
+                }
+                else {
+                    // save the catalog data
+                    setCatalogData(ret_val);
+                }
             }
-            else {
-                // save the catalog data
-                setCatalogData(data);
-            }
+            else
+                resetForm();
 
             // return something
             return true;
-        },
-
-        refetchOnWindowFocus: false
+        }, refetchOnWindowFocus: false
     });
 
     /**
-     * this will build the data url and will cause a DB hit
+     * this will build the data url and will cause a web service/DB hit
      */
     useEffect(() => {
         // build the new data url
@@ -163,49 +195,78 @@ export const SynopticTabForm = () => {
         return !dropDownData['run_dates'].includes(date.toISOString().split("T")[0]);
     };
 
+    const [error, setError] = React.useState(null);
+
+    const errorMessage = React.useMemo(() => {
+        switch (error) {
+            case 'minDate': {
+                return 'Please select a date after 01/01/2020';
+            }
+
+            case 'invalidDate': {
+                return 'Your date is not valid';
+            }
+
+            default: {
+                return '';
+            }
+        }
+    }, [error]);
+
     /**
      * return the rendered component
      */
     return (
         <Fragment>
-            <form name={"Synoptic"} onSubmit={formSynopticSubmit}>
-                <Stack spacing={1}>
-                    <LocalizationProvider dateAdapter={AdapterDayjs}>
+            <form name={ "Synoptic" } onSubmit={ formSynopticSubmit }>
+                <Stack spacing={ 1 }>
+                    <LocalizationProvider dateAdapter={ AdapterDayjs }>
                         <DatePicker
+                            //value={{ synopticDate }}
+                            //defaultValue={ new Date() }
+                            onError={(newError) => setError(newError)}
+                            //disableFuture
                             name="synoptic-date"
-                            shouldDisableDate={disableDate}
-                            slotProps={{textField: {size: 'small'}, field: {clearable: true}}}
+                            shouldDisableDate={ disableDate }
+                            //minDate={ new Date(2020, 0, 1) }
+                            slotProps={{
+                                textField: { size: 'small' },
+                                helperText: errorMessage,
+                                field: { clearable: true },
+                                actionBar: { actions: ['clear'] },
+                            }}
+
                             onChange={(newValue) => {
-                                setSynopticDate(newValue.$d);
+                                setChangedSynopticDate(newValue);
                             }}/>
                     </LocalizationProvider>
 
-                    <Select name="synoptic-cycle" value={synopticCycle} placeholder="Please select a cycle" onChange={(e, newValue) => {
+                    <Select name="synoptic-cycle" value={ synopticCycle } placeholder="Please select a cycle" onChange={ (e, newValue) => {
                         setSynopticCycle(newValue);
                     }}>
-                        <DropDownOptions data={dropDownData} type={'cycles'}/>
+                        <DropDownOptions data={ dropDownData } type={ 'cycles' }/>
                     </Select>
 
-                    <Select name="synoptic-grid" value={synopticGrid} placeholder="Please select a grid" onChange={(e, newValue) => {
+                    <Select name="synoptic-grid" value={ synopticGrid } placeholder="Please select a grid" onChange={ (e, newValue) => {
                         setSynopticGrid(newValue);
                     }}>
-                        <DropDownOptions data={dropDownData} type={'grid_types'}/>
+                        <DropDownOptions data={ dropDownData } type={ 'grid_types' }/>
                     </Select>
 
-                    <Select name="synoptic-instance" value={synopticInstance} placeholder="Please select an instance" onChange={(e, newValue) => {
+                    <Select name="synoptic-instance" value={ synopticInstance } placeholder="Please select an instance" onChange={ (e, newValue) => {
                         setSynopticInstance(newValue);
                     }}>
-                        <DropDownOptions data={dropDownData} type={'instance_names'}/>
+                        <DropDownOptions data={ dropDownData } type={'instance_names'}/>
                     </Select>
 
                     <Button type="submit">Submit</Button>
-                    <Button type="reset" onClick={resetForm}>Reset</Button>
+                    <Button type="reset" onClick={ resetForm }>Reset</Button>
                 </Stack>
 
                 <Divider sx={{m: 2}}/>
 
                 <Stack>
-                    {<CatalogItems data={catalogData} isTropical={false}/>}
+                    { <CatalogItems data={ catalogData } isTropical={ false }/> }
                 </Stack>
             </form>
         </Fragment>
