bcdaed25dedbadaf34a04467737a71b005bd424d
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-10-02 11:03:40 -0400

    using the state enabled way of resetting the compare mode

--- src/components/trays/remove/reset-compare.js
+++ src/components/trays/remove/reset-compare.js
@@ -6,19 +6,28 @@ import { useLayers } from "@context";
  * component that handles the removal of styles from
  * local storage ADCIRC raster layers
  *
- * @returns {JSX.Element}
+ * @returns JSX.Element
  * @constructor
  */
 export const ResetCompare = () => {
     // get the context for the compare layers view
     const {
-        resetCompare
+        resetCompare, setInCompareMode
     } = useLayers();
+
+    /**
+     * method to use the state enabled way to reset compare mode
+     *
+     */
+    const reset = () => {
+        resetCompare();
+        setInCompareMode(false);
+    };
 
     // render the button
     return (
         <Fragment>
-            <Button color="primary" onClick={ () => resetCompare() }>Remove compare mode</Button>
+            <Button color="primary" onClick={ () => reset() }>Remove compare mode</Button>
         </Fragment>
   );
 };
