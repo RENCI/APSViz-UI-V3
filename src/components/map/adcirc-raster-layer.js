372abd231e69d013a0899c714666d47d37e2b476
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-10-17 09:00:44 -0400

    tidying up the addition of the geo point data

--- src/components/map/adcirc-raster-layer.js
+++ src/components/map/adcirc-raster-layer.js
@@ -21,40 +21,35 @@ export const AdcircRasterLayer = (layer) => {
     // create a callback to handle a map click event
     const onClick = useCallback ( (e) => {
         // create an id for the point
-        const id = e.latlng.lat + ", " + e.latlng.lng;
+        const id = e.latlng.lng + ", " + e.latlng.lat;
 
-        // does this point already exist
-        if(Object.values(selectedObservations).indexOf(id) === -1) {
-            // create a marker target icon around the observation clicked
-            markClicked(map, e, id);
+        // create a marker target icon around the observation clicked
+        markClicked(map, e, id);
 
-            console.log(id);
+        // get the FQDN of the UI data server
+        const data_url = `${ getNamespacedEnvParam('REACT_APP_UI_DATA_URL') }`;
 
-            // get the FQDN of the UI data server
-            const data_url = `${ getNamespacedEnvParam('REACT_APP_UI_DATA_URL') }`;
+        // create a set of properties for this object
+        const pointProps =
+            {
+                "station_name": "Point (lon, lat): " + id,
+                "lat": e.latlng.lat,
+                "lon": e.latlng.lng,
+                "location_name": "Point (lon, lat): " + id,
+                "model_run_id": "4489-2024101606-gfsforecast",
+                "data_source": "GFSFORECAST_NCSC_SAB_V1.23",
+                "source_name": "adcirc",
+                "source_instance": "ncsc123_gfs_da",
+                "source_archive": "RENCI",
+                "forcing_metclass": "synoptic",
+                "location_type": "ocean",
+                "grid_name": "NCSC_SAB_V1.23",
+                "csvurl": data_url + "get_geo_point_data?lon=" + e.latlng.lng + "&lat=" + e.latlng.lat + "&url=https%3A%2F%2Ftds.renci.org%2Fthredds%2FdodsC%2F2024%2Fgfs%2F2024041712%2FNCSC_SAB_v1.23%2Fht-ncfs.renci.org%2Fncsc123_gfs_sb55.01%2Fgfsforecast%2Ffort.63.nc&ensemble=nowcast",
+                "id": id
+            };
 
-            // create a set of properties for this object
-            const pointProps =
-                {
-                    "station_name": "Ocean (lat, lon): " + id,
-                    "lat": e.latlng.lat,
-                    "lon": e.latlng.lng,
-                    "location_name": "Ocean (lat, lon): " + id,
-                    "model_run_id": "4489-2024101606-gfsforecast",
-                    "data_source": "GFSFORECAST_NCSC_SAB_V1.23",
-                    "source_name": "adcirc",
-                    "source_instance": "ncsc123_gfs_da",
-                    "source_archive": "RENCI",
-                    "forcing_metclass": "synoptic",
-                    "location_type": "ocean",
-                    "grid_name": "NCSC_SAB_V1.23",
-                    "csvurl": data_url + "get_geo_point_data?lon=" + e.latlng.lng + "&lat=" + e.latlng.lat + "&url=https%3A%2F%2Ftds.renci.org%2Fthredds%2FdodsC%2F2024%2Fgfs%2F2024041712%2FNCSC_SAB_v1.23%2Fht-ncfs.renci.org%2Fncsc123_gfs_sb55.01%2Fgfsforecast%2Ffort.63.nc&ensemble=nowcast",
-                    "id": id
-                };
-
-            // populate selectedObservations list with the newly selected observation point
-            setSelectedObservations(previous => [...previous, pointProps]);
-        }
+        // populate selectedObservations list with the newly selected observation point
+        setSelectedObservations(previous => [...previous, pointProps]);
     });
 
     // assign the map click event
@@ -62,8 +57,7 @@ export const AdcircRasterLayer = (layer) => {
 
     // get the observation point selected state
     const {
-        selectedObservations,
-        setSelectedObservations,
+        setSelectedObservations
     } = useLayers();
 
    useEffect(() => {
