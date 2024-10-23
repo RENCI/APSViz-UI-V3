34088ebf6d7f91a3049bb8dd0ce928f86660abd3
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2024-10-23 17:05:25 -0400

    adding the default instance name collection for the web service, error handling

--- src/components/config/config.js
+++ src/components/config/config.js
@@ -1,26 +1,95 @@
-import React, { Fragment, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLayers } from "@context";
+import { getNamespacedEnvParam } from "@utils";
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 
+/**
+ * gets the default instance name for startup layers
+ *
+ */
+export const getDefaultInstanceName = () => {
+    // init the return
+    let ret_val = '';
+
+    // get the state variable that suppresses using the instance name
+    const {
+        defaultInstanceName
+    } = useLayers();
+
+    // if there is a valid default instance name
+    if (!defaultInstanceName.includes('Error') && defaultInstanceName.length) {
+        // build the extended query string
+        ret_val = '&instance_name=' + defaultInstanceName;
+    }
+
+    // return the query string addition
+    return ret_val;
+};
+
+/**
+ * handles getting the default instance name
+ *
+ * @returns JSX.Element
+ * @constructor
+ */
 export const Config = () => {
     // get the message alert details from state
     const { setDefaultInstanceName } = useLayers();
 
-    useEffect (() => {
-        // get the instance name from UI data services
-        const instance_name = getDefaultInstanceName();
+    // use this to trigger the data retrieval
+    const [ dataUrl, setDataUrl ] = useState(null);
 
-        // if the retrieval successful
-        if (!instance_name.includes('Error')) {}
-            // set the default instance name
-            setDefaultInstanceName(instance_name);
-        }, []
-    );
+    /**
+     * create a url to get the instance name
+     */
+    useEffect( () => {
+        // get the site branding for the query string
+        const theUrl = 'get_ui_instance_name?reset=false&site_branding=' + (window.location.href.includes('nopp') ? 'NOPP' : 'APSViz');
 
-    const getDefaultInstanceName = () => {
-        return 'ec95d_dev_test';
-    };
+        // set the data url. this will spawn a data request
+        setDataUrl(getNamespacedEnvParam('REACT_APP_UI_DATA_URL') + theUrl);
+    }, [] );
 
-    return(
-        <Fragment></Fragment>
-    );
+    /**
+     *  grab the default instance name
+     */
+    useQuery( {
+        // specify the data key and url to use
+        queryKey: ['get_ui_instance_name', dataUrl],
+
+        // create the function to call for data
+        queryFn: async () => {
+            // create the authorization header
+            const requestOptions = {
+                method: 'GET',
+                headers: { Authorization: `Bearer ${ getNamespacedEnvParam('REACT_APP_UI_DATA_TOKEN') }`}
+            };
+
+            // make the call to get the data
+            const ret_val = await axios
+                // make the call to get the data
+                .get(dataUrl, requestOptions)
+                // use the data returned
+                .then (( response ) => {
+                    // return the data
+                    return response.data;
+                })
+                .catch (( error ) => {
+                    // make sure we do not render anything
+                    return error.response.status;
+                });
+
+            // if the retrieval did not have an issue
+            if (typeof ret_val === 'string' && !ret_val.includes('Error'))
+                // save the instance name value
+                setDefaultInstanceName(ret_val);
+            else
+                // blank the instance name on any http or data gathering error.
+                setDefaultInstanceName('');
+
+            // return something
+            return true;
+        }, refetchOnWindowFocus: false
+    });
 };
