b3d44d827091c1f7d251f0d5f0ae49f086bd3966
Lisa Stillwell <lisa@renci.org>
2024-11-12 10:06:15 -0500

    #296 correct mps designation for maxwvel in default style

--- src/utils/default-styles.js
+++ src/utils/default-styles.js
@@ -56,27 +56,27 @@ export const maxwvelStyle = '<?xml version="1.0" encoding="UTF-8"?><sld:StyledLa
               </sld:GrayChannel> \
             </sld:ChannelSelection> \
             <sld:ColorMap> \
-              <sld:ColorMapEntry color="#3E26A9" quantity="0.0" label="0.0 m/s"/> \
-              <sld:ColorMapEntry color="#4433CD" quantity="3.0" label="3.0 m/s"/> \
-              <sld:ColorMapEntry color="#4743E8" quantity="6.0" label="6.0 m/s"/> \
-              <sld:ColorMapEntry color="#4755F6" quantity="9.0" label="9.0 m/s"/> \
-              <sld:ColorMapEntry color="#4367FE" quantity="12.0" label="12.0 m/s"/> \
-              <sld:ColorMapEntry color="#337AFD" quantity="15.0" label="15.0 m/s"/> \
-              <sld:ColorMapEntry color="#2D8CF4" quantity="18.0" label="18.0 m/s"/> \
-              <sld:ColorMapEntry color="#259CE8" quantity="21.0" label="21.0 m/s"/> \
-              <sld:ColorMapEntry color="#1BAADF" quantity="24.0" label="24.0 m/s"/> \
-              <sld:ColorMapEntry color="#04B6CE" quantity="27.0" label="27.0 m/s"/> \
-              <sld:ColorMapEntry color="#12BEB9" quantity="30.0" label="30.0 m/s"/> \
-              <sld:ColorMapEntry color="#2FC5A2" quantity="33.0" label="33.0 m/s"/> \
-              <sld:ColorMapEntry color="#47CB86" quantity="36.0" label="36.0 m/s"/> \
-              <sld:ColorMapEntry color="#71CD64" quantity="39.0" label="39.0 m/s"/> \
-              <sld:ColorMapEntry color="#9FC941" quantity="42.0" label="42.0 m/s"/> \
-              <sld:ColorMapEntry color="#C9C128" quantity="45.0" label="45.0 m/s"/> \
-              <sld:ColorMapEntry color="#EBBB30" quantity="48.0" label="48.0 m/s"/> \
-              <sld:ColorMapEntry color="#FFC13A" quantity="51.0" label="51.0 m/s"/> \
-              <sld:ColorMapEntry color="#FBD42E" quantity="54.0" label="54.0 m/s"/> \
-              <sld:ColorMapEntry color="#F5E824" quantity="57.0" label="57.0 m/s"/> \
-              <sld:ColorMapEntry color="#FAFB14" quantity="60.0" label="60.0 m/s"/> \
+              <sld:ColorMapEntry color="#3E26A9" quantity="0.0" label="0.0 mps"/> \
+              <sld:ColorMapEntry color="#4433CD" quantity="3.0" label="3.0 mps"/> \
+              <sld:ColorMapEntry color="#4743E8" quantity="6.0" label="6.0 mps"/> \
+              <sld:ColorMapEntry color="#4755F6" quantity="9.0" label="9.0 mps"/> \
+              <sld:ColorMapEntry color="#4367FE" quantity="12.0" label="12.0 mps"/> \
+              <sld:ColorMapEntry color="#337AFD" quantity="15.0" label="15.0 mps"/> \
+              <sld:ColorMapEntry color="#2D8CF4" quantity="18.0" label="18.0 mps"/> \
+              <sld:ColorMapEntry color="#259CE8" quantity="21.0" label="21.0 mps"/> \
+              <sld:ColorMapEntry color="#1BAADF" quantity="24.0" label="24.0 mps"/> \
+              <sld:ColorMapEntry color="#04B6CE" quantity="27.0" label="27.0 mps"/> \
+              <sld:ColorMapEntry color="#12BEB9" quantity="30.0" label="30.0 mps"/> \
+              <sld:ColorMapEntry color="#2FC5A2" quantity="33.0" label="33.0 mps"/> \
+              <sld:ColorMapEntry color="#47CB86" quantity="36.0" label="36.0 mps"/> \
+              <sld:ColorMapEntry color="#71CD64" quantity="39.0" label="39.0 mps"/> \
+              <sld:ColorMapEntry color="#9FC941" quantity="42.0" label="42.0 mps"/> \
+              <sld:ColorMapEntry color="#C9C128" quantity="45.0" label="45.0 mps"/> \
+              <sld:ColorMapEntry color="#EBBB30" quantity="48.0" label="48.0 mps"/> \
+              <sld:ColorMapEntry color="#FFC13A" quantity="51.0" label="51.0 mps"/> \
+              <sld:ColorMapEntry color="#FBD42E" quantity="54.0" label="54.0 mps"/> \
+              <sld:ColorMapEntry color="#F5E824" quantity="57.0" label="57.0 mps"/> \
+              <sld:ColorMapEntry color="#FAFB14" quantity="60.0" label="60.0 mps"/> \
             </sld:ColorMap> \
             <sld:ContrastEnhancement/> \
           </sld:RasterSymbolizer> \
