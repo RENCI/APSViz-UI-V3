f2153ade6c5afe75acd8f3ed1b39cf859b6419f8
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2025-01-03 16:11:29 -0500

    refactoring for clarity, making the page a form

--- src/components/auth/add-user.js
+++ src/components/auth/add-user.js
@@ -1,20 +1,20 @@
 import React, { useState } from "react";
+import { Button, Divider, Typography, Input, Box, Tooltip } from '@mui/joy';
 import { useAuth } from "@auth";
 import { getNamespacedEnvParam } from "@utils";
-import axios from 'axios';
-import { Button, Divider, Typography, Input, Box, Tooltip } from '@mui/joy';
-import isaac from "isaac";
 import { Branding } from "@control-panel";
+import axios from 'axios';
 
 // load the encryption library
-const bcrypt = require('react-native-bcrypt');
+import bcrypt from "react-native-bcrypt";
+import isaac from "isaac";
 
 // override using unsecure math.random when generating hashes
-bcrypt.setRandomFallback = (len) => {
-    const buf = new Uint8Array(len);
+bcrypt.setRandomFallback((len) => {
+	const buf = new Uint8Array(len);
 
-    return buf.map(() => Math.floor(isaac.random() * 256));
-};
+	return buf.map(() => Math.floor(isaac.random() * 256));
+});
 
 /**
  * page to collect the user credentials and verify them
@@ -34,7 +34,7 @@ export const AddUser = () => {
     const [lastNameValue, setLastNameValue] = useState('');
 
     // redirect to the main page on successful account addition
-    const {addUser} = useAuth();
+    const { login, addUser } = useAuth();
 
     /**
      * Returns the user profile details
@@ -77,39 +77,38 @@ export const AddUser = () => {
         // init the return value
         let ret_val = true;
 
-        // create a regex to validate the email address
+        // create a regex to validate the email address and password
         const email_regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         const pwd_regex = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
 
-        // all fields are mandatory, the email must be legit
-        if (email && first_name && last_name && password && new_password) {
-            // if a valid email address entered
-            if (!email_regex.test(email)) {
-                // let the user know
-                setError('The email address you have entered is invalid.');
+        // if a valid email address entered
+        if (!email_regex.test(email)) {
+            // let the user know
+            setError('The email address you have entered is invalid.');
 
-                setEmailValue('');
+            setEmailValue('');
 
-                ret_val = false;
-            }
-            // if the password entries do not match
-            else if (password !== new_password) {
-                // let the user know
-                setError('The passwords you entered do not match.');
+            ret_val = false;
+        }
+        // if the password entries do not match
+        else if (password !== new_password) {
+            // let the user know
+            setError('The passwords you entered do not match.');
 
-                setPasswordValue('');
-                setNewPasswordValue('');
+            setPasswordValue('');
+            setNewPasswordValue('');
 
-                ret_val = false;
-            } else if (!pwd_regex.test(password)) {
-                // let the user know
-                setError('Legitimate passwords are between 7 to 15 characters which contain at least one numeric digit and a special character.');
+            ret_val = false;
+        }
+        // make sure the password is formatted properly
+        else if (!pwd_regex.test(password)) {
+            // let the user know
+            setError('Legitimate passwords are between 7 to 15 characters which contain at least one numeric digit and a special character.');
 
-                setPasswordValue('');
-                setNewPasswordValue('');
+            setPasswordValue('');
+            setNewPasswordValue('');
 
-                ret_val = false;
-            }
+            ret_val = false;
         }
 
         // return to the caller
@@ -131,7 +130,8 @@ export const AddUser = () => {
             // return the query string
             return `${getNamespacedEnvParam('REACT_APP_UI_DATA_URL')}` +
                 `add_user?email=${email}&password_hash=${getPasswordHash(password)}&role_id=2&details=${getUserDetails(first_name, last_name)}`;
-        } else {
+        }
+        else {
             return false;
         }
     };
@@ -141,7 +141,9 @@ export const AddUser = () => {
      *
      * @returns {Promise<void>}
      */
-    const onAddUserClicked = async () => {
+    const onAddUserClicked = async (e) => {
+        e.preventDefault();
+
         // generate the query string
         const data_url = getQueryString(emailValue, firstNameValue, lastNameValue, passwordValue);
 
@@ -173,9 +175,13 @@ export const AddUser = () => {
             // continue to validate the credentials
             else {
                 // if the call successful and it is the correct password
-                if (ret_val['success'])
+                if (ret_val['success']) {
                     // save the new user profile
                     addUser();
+
+                    // go to the login page
+                    login();
+                }
                 else {
                     // show the error
                     setError('The user name may already exist.');
@@ -194,69 +200,75 @@ export const AddUser = () => {
                 transform: 'translate(-50%, -50%)',
                 width: '350px'
             }}>
-
-            <Box bgcolor="white" sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                border: 3,
-                borderColor: '#245F97',
-                gap: 1 }}>
-
-                <Box sx={{
-                    m: 1,
+            <form name={"add-user"} onSubmit={ onAddUserClicked }>
+                <Box bgcolor="white" sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
                     border: 3,
-                    borderColor: '#245F97'
+                    borderColor: '#245F97',
+                    gap: 1
                 }}>
-                    <Branding/>
-                </Box>
 
-                { error && <Typography sx={{ ml: 1, mr: 1, mb: 1, display: 'flex', fontSize: 15, color: 'red' }}>{ error }</Typography> }
+                    <Box sx={{
+                        m: 1,
+                        border: 3,
+                        borderColor: '#245F97'
+                    }}>
+                        <Branding/>
+                    </Box>
 
-                <Input
-                    sx={{ width: '75%' }}
-                    value={ firstNameValue }
-                    onChange={ e => setFirstNameValue(e.target.value) }
-                    placeholder="First name"/>
+                    {error && <Typography sx={{ml: 1, mr: 1, mb: 1, display: 'flex', fontSize: 15, color: 'red'}}>{error}</Typography>}
 
-                <Input
-                    sx={{ width: '75%' }}
-                    value={ lastNameValue }
-                    onChange={ e => setLastNameValue(e.target.value) }
-                    placeholder="Last name"/>
-
-                <Input
-                    sx={{ width: '75%' }}
-                    value={ emailValue }
-                    onChange={ e => setEmailValue(e.target.value) }
-                    placeholder="Email address (user name)"/>
-
-                <Tooltip
-                    sx={{ width: '300px'}}
-                    title={"Legitimate passwords are between 7 to 15 characters which contain at least one numeric digit and a special character."}
-                >
                     <Input
-                        sx={{ width: '75%' }}
+                        sx={{width: '75%'}}
+                        value={firstNameValue}
+                        onChange={e => setFirstNameValue(e.target.value)}
+                        placeholder="First name"/>
+
+                    <Input
+                        sx={{width: '75%'}}
+                        value={lastNameValue}
+                        onChange={e => setLastNameValue(e.target.value)}
+                        placeholder="Last name"/>
+
+                    <Input
+                        sx={{width: '75%'}}
+                        value={emailValue}
+                        onChange={e => setEmailValue(e.target.value)}
+                        placeholder="User name (Email address)"/>
+
+                    <Tooltip
+                        sx={{width: '300px'}}
+                        title={"Legitimate passwords are between 7 to 15 characters which contain at least one numeric digit and a special character."}
+                    >
+                        <Input
+                            sx={{width: '75%'}}
+                            type="password"
+                            value={passwordValue}
+                            onChange={e => setPasswordValue(e.target.value)}
+                            placeholder="Password"/>
+                    </Tooltip>
+
+                    <Input
+                        sx={{width: '75%'}}
                         type="password"
-                        value={ passwordValue }
-                        onChange={ e => setPasswordValue(e.target.value) }
-                        placeholder="Password"/>
-                </Tooltip>
+                        value={newPasswordValue}
+                        onChange={e => setNewPasswordValue(e.target.value)}
+                        placeholder="Verify your password"/>
 
-                <Input
-                    sx={{ width: '75%' }}
-                    type="password"
-                    value={ newPasswordValue }
-                    onChange={ e => setNewPasswordValue(e.target.value) }
-                    placeholder="Verify your password"/>
+                    <Divider sx={{m: 1}}/>
 
-                <Divider sx={{ m: 1 }}/>
-
-                <Button sx={{ mb: 1, width: '90%' }}
-                    disabled={ !emailValue || !firstNameValue || !lastNameValue || !passwordValue || !newPasswordValue }
-                    onClick={ onAddUserClicked }>Sign me up
-                </Button>
-            </Box>
+                    <Button
+                        type="submit"
+                        sx={{mb: 1, width: '90%'}}
+                        disabled={!emailValue || !firstNameValue || !lastNameValue || !passwordValue || !newPasswordValue}
+                        // onClick={ onAddUserClicked }
+                    >
+                        Sign me up
+                    </Button>
+                </Box>
+            </form>
         </div>
     );
 };
