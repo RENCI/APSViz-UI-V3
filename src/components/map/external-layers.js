73b67cc67d49f169ed98c2bd83c0b4550ae66f03
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2025-01-17 15:14:43 -0500

    refactoring to support using data in state

--- src/components/map/external-layers.js
+++ src/components/map/external-layers.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { WMSTileLayer } from 'react-leaflet';
-import { externalLayerList } from '@utils/external-layer-list';
+import { useLayers } from '@context/map-context';
 
 // external layers are other types of related map layers
 // that are provided by external map services other than MetGet
@@ -8,9 +8,13 @@ import { externalLayerList } from '@utils/external-layer-list';
 // external layer can be features or coverages (i.e. rasters)
 // the last layer selected, to be displayed, from list of
 // external layers will be moved to the top of the external
-// layers list and displayed imeediately
+// layers list and displayed immediately
 
 export const ExternalLayers = () => {
+    // storage from state for rendering external layers
+    const {
+        externalLayers
+    } = useLayers();
 
     //const sst_url = "https://coastwatch.noaa.gov/erddap/wms/noaacwBLENDEDsstDNDaily/request";
     //const sst_layer = "noaacwBLENDEDsstDNDaily:analysed_sst";
@@ -21,36 +25,24 @@ export const ExternalLayers = () => {
     //const sst_url = "https://coastwatch.noaa.gov/erddap/wms/noaacwecnAVHRRmultisatsstEastCoast3Day/request";
     //const sst_layer = "noaacwecnAVHRRmultisatsstEastCoast3Day:sst";
 
-    const sst_url = "https://tds.maracoos.org/wms";
-    const sst_layer = "GOES_SST/sst";
-
-    const wmsLayerParams = {
-        format: "image/png",
-        transparent: true,
-        srs: "EPSG:3857",
-        styles: "raster/GHRSST_L4_MUR_Sea_Surface_Temperature",
-    };
-    
-    // externalLayerList.map((layer) => {
-    //     console.log(layer.url);
-    //     console.log(layer.name);
-    //     console.log(layer.params);
-    // });
-
-    return (
-        <>
-        {externalLayerList.map((layer) => (
-            <WMSTileLayer
-                /* key={layer.name}
-                url={layer.url}
-                layers={layer.name}
-                params={layer.params} */
-                key={sst_layer}
-                url={sst_url}
-                layers={sst_layer}
-                params={wmsLayerParams}
-            />
-        ))};
-        </>
-    );
+    if (externalLayers != null) {
+        return (
+            <Fragment>
+                {
+                    externalLayers
+                    .filter(item => item.state.visible === true)
+                    .map(
+                        (layer) => (
+                            <WMSTileLayer
+                                key={layer.name}
+                                url={layer.url}
+                                layers={layer.layer}
+                                params={layer.params}
+                            />
+                        )
+                    )
+                }
+            </Fragment>
+        );
+    }
 };
