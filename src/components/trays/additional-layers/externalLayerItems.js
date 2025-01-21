f5222e9a91d988e0028256f50fa6bfe42f8f8ef1
Phil Owen <19691521+PhillipsOwen@users.noreply.github.com>
2025-01-21 08:28:24 -0500

    adding accordions for the layers to show metadata

--- src/components/trays/additional-layers/externalLayerItems.js
+++ src/components/trays/additional-layers/externalLayerItems.js
@@ -1,11 +1,17 @@
-import React, { Fragment } from "react";
-import { AccordionGroup, Accordion, AccordionSummary, AccordionDetails,
-    Stack, Checkbox, Typography, Divider, Tab, Tabs, TabPanel, TabList, Box } from '@mui/joy';
+import React, { Fragment, useState } from "react";
+import {
+    AccordionGroup, Accordion, AccordionDetails, Button, Stack,
+    Checkbox, Typography, Tab, Tabs, TabPanel, TabList, Box } from '@mui/joy';
+import { KeyboardArrowDown as ExpandIcon } from '@mui/icons-material';
 import { useLayers } from "@context/map-context";
 import PropTypes from 'prop-types';
+import {ActionButton} from "@components/buttons";
 
 // set component prop types
-ExternalLayerItems.propTypes = { data: PropTypes.any };
+ExternalLayerItems.propTypes = {
+    data: PropTypes.any,
+    state: PropTypes.any
+};
 
 /**
  * renders a list of available external layers from the DB.
@@ -18,6 +24,9 @@ export default function ExternalLayerItems(data) {
     const {
         externalLayers, setExternalLayers
     } = useLayers();
+
+    // set some state for the accordion expansion view
+    const [expandedAccordion, setExpandedAccordion] = useState([]);
 
     /**
      * checks to see if this checkbox needs to be set based on the layers currently in layer state.
@@ -36,6 +45,20 @@ export default function ExternalLayerItems(data) {
                 return layer[0].state.visible;
         }
     };
+
+    /**
+     * clears the checkboxes on the tab
+     */
+    function resetCheckBoxes() {
+        // loop through the layers
+        externalLayers.map(layer => {
+            // if this later is set to visible
+            if (layer.state.visible) {
+                // toggle the layer's visibility
+                toggleLayerVisibility(layer.name);
+            }
+        });
+    }
 
     /**
      * handles updating the visibility of an external layers on the map surface
@@ -66,7 +89,65 @@ export default function ExternalLayerItems(data) {
     };
 
     /**
-     * render the external layers
+     * toggles the accordion view in state
+     *
+     * @param id
+     */
+    const toggleAccordionView = (id) => {
+        // get a copy of the state list
+        let newExpandedAccordion = [ ...expandedAccordion ];
+
+        // find the index of this item in state
+        const index = expandedAccordion.findIndex(l => l.id === id);
+
+        // add a new one to the list if it doesn't exist
+        if (index === -1) {
+            // add a new item to the state list
+            newExpandedAccordion = [...expandedAccordion, { id: id, enabled: true }];
+
+            // update the state list
+            setExpandedAccordion(newExpandedAccordion);
+        } else {
+            // get the target instance in the state array
+            const alteredExpandedAccordion = newExpandedAccordion[index];
+
+            // toggle the view state of the accordion
+            alteredExpandedAccordion.enabled = !alteredExpandedAccordion.enabled;
+
+            // rebuild the list of accordions with the altered view state
+            setExpandedAccordion([
+                ...newExpandedAccordion.slice(0, index),
+                { alteredExpandedAccordion },
+                ...newExpandedAccordion.slice(index + 1)
+            ]);
+        }
+    };
+
+    /**
+     * gets the current state of the accordion
+     *
+     * @param id
+     * @returns {boolean}
+     */
+    const getAccordionState = (id) => {
+        // init the return
+        let ret_val = false;
+
+        // find the index of this item in state
+        const index = expandedAccordion.findIndex(l => l.id === id);
+
+        // add a new one to the list if it doesn't exist
+        if (index !== -1) {
+            // get the state
+            ret_val = expandedAccordion[index].enabled;
+        }
+
+        // return the state
+        return ret_val;
+    };
+
+    /**
+     * render the external layers tray contents
      */
     // do not render if there is no data
     if (data.data != null) {
@@ -99,72 +180,96 @@ export default function ExternalLayerItems(data) {
             // render the results of the data query
             return (
                 <Fragment>
-                    {
-                        // loop through the external layers and create checkbox selections grouped by the source
-                        externalLayers
-                        // group by the source name
-                        .filter((val, idx, self) => (
-                            idx === self.findIndex((t)=> ( t['source'] === val['source']) ))
-                        )
-                        // output sources
-                        .map(
-                            (layer, itemIndex) => (
-                                <Stack key={ itemIndex } spacing={ .1 } direction="column" gap={ .1 }>
-                                    <Typography level="sm" sx={{ fontWeight: 'bold' }}> { layer['source'] }</Typography>
-                                    {
-                                        // output checkboxes for each layer name/url
-                                        externalLayers
+                    <AccordionGroup
+                        disableDivider
+                        // variant="soft"
+                        sx={{
+                            '.MuiAccordionDetails-content': { p: .5 },
+                            '.MuiAccordionSummary-button': {  alignItems: 'center' }
+                        }}>
+                        {
+                            // loop through the external layers and create checkbox selections grouped by the source
+                            externalLayers
+                            // group by the source name
+                            .filter((val, idx, self) => (
+                                idx === self.findIndex((t)=> ( t['source'] === val['source']) ))
+                            )
+                            // output sources
+                            .map(
+                                (layer, itemIndex) => (
+                                    <Stack key={ itemIndex } spacing={ 0 } direction="column" gap={ 0 }>
+                                        <Typography level="body-sm" sx={{ mb: 1, fontWeight: 'bold' }}> { layer['source'] }</Typography>
+                                        {
+                                            // output checkboxes for each layer name/url
+                                            externalLayers
                                             // get the layer names for this source type
                                             .filter(item => item.source === layer.source)
                                             // output the checkboxes
                                             .map ((layer, itemIndex) =>
-                                                <AccordionGroup key={ itemIndex } sx={{ maxWidth: 415, size: "sm", variant: "soft", p: 0 }}>
-                                                    <Accordion sx={{ p: 0 }}>
-                                                        <AccordionSummary>
+                                                <Accordion
+                                                    key={ itemIndex }
+                                                    sx={{ p: 0 }}
+                                                    expanded={ getAccordionState(layer['name']) }
+                                                    onChange={ () => toggleAccordionView(layer['name']) }>
+
+                                                    <Stack
+                                                        direction="row"
+                                                        justifyContent="space-between"
+                                                        alignItems="stretch"
+                                                        gap={ 2 }
+                                                        >
                                                             <Checkbox
                                                                 size="sm"
                                                                 checked={ getCheckedState( layer.name ) }
                                                                 label={ <Typography sx={{ fontSize: "xs" }}> { layer['name'] } </Typography> }
                                                                 onChange={ () => toggleLayerVisibility(layer['name']) }
                                                             />
-                                                        </AccordionSummary>
 
-                                                        <AccordionDetails>
-                                                            <Tabs defaultValue={0}>
-                                                                <Stack
-                                                                    direction="row"
-                                                                    justifyContent="space-between"
-                                                                >
-                                                                    <TabList size="sm" sx={{ flex: 1 }}>
-                                                                        <Tab variant="plain" color="primary">
-                                                                            Metadata
-                                                                        </Tab>
-                                                                    </TabList>
-                                                                </Stack>
+                                                            <ActionButton onClick={ () => toggleAccordionView( layer['name']) }>
+                                                                <ExpandIcon
+                                                                    fontSize="sm"
+                                                                    sx={{ transform: getAccordionState(layer['name']) ? 'rotate(180deg)' : 'rotate(0)',
+                                                                        transition: 'transform 100ms' }} />
+                                                            </ActionButton>
+                                                    </Stack>
 
-                                                                <TabPanel value={ 0 }>
-                                                                    <Box component="pre" sx={{
-                                                                        fontSize: '75%',
-                                                                        color: 'text.primary',
-                                                                        backgroundColor: 'transparent',
-                                                                        overflowX: 'auto',
-                                                                        m: 0, p: 1,
-                                                                        height: '100px',
-                                                                        }}>
-                                                                            { JSON.stringify(layer, null, 2) }
-                                                                    </Box>
-                                                                </TabPanel>
-                                                            </Tabs>
-                                                        </AccordionDetails>
-                                                    </Accordion>
-                                                </AccordionGroup>
+                                                    <AccordionDetails>
+                                                        <Tabs defaultValue={0}>
+                                                            <Stack
+                                                                direction="row"
+                                                                justifyContent="space-between"
+                                                            >
+                                                                <TabList size="sm" sx={{ flex: 1 }}>
+                                                                    <Tab variant="plain" color="primary">
+                                                                        Metadata
+                                                                    </Tab>
+                                                                </TabList>
+                                                            </Stack>
+
+                                                            <TabPanel value={ 0 }>
+                                                                <Box component="pre" sx={{
+                                                                    fontSize: '75%',
+                                                                    color: 'text.primary',
+                                                                    backgroundColor: 'transparent',
+                                                                    overflowX: 'auto',
+                                                                    m: 0, p: 1,
+                                                                    height: '100px',
+                                                                    }}>
+                                                                        { JSON.stringify(layer, null, 2) }
+                                                                </Box>
+                                                            </TabPanel>
+                                                        </Tabs>
+                                                    </AccordionDetails>
+                                                </Accordion>
                                             )
-                                    }
-                                   <Divider />
-                                </Stack>
+                                        }
+                                    </Stack>
+                                )
                             )
-                        )
-                    }
+                        }
+                    </AccordionGroup>
+
+                    <Button type="reset" onClick={ resetCheckBoxes }>Reset</Button>
                 </Fragment>
             );
         }
